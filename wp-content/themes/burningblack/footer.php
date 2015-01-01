8b254edcbf509d9cff5fc5d59c96dda5114792c4
Enrico Antonello <eantonello@h-art.com>
2015-01-01 15:03:45 +0100

    adjustments

--- wp-content/themes/burningblack/footer.php
+++ wp-content/themes/burningblack/footer.php
@@ -1,16 +1,10 @@
 <footer class="row footer">
 
 	<ul class="small-block-grid-2 large-block-grid-4">
-		<li><img src="<?php echo get_template_directory_uri().'/assets/img/images/logos/esp.png' ?>" /></li>
-		<li><img src="<?php echo get_template_directory_uri().'/assets/img/images/logos/ibanez.png' ?>" /></li>
-		<li><img src="<?php echo get_template_directory_uri().'/assets/img/images/logos/dragoon.png' ?>" /></li>
-		<li><img src="<?php echo get_template_directory_uri().'/assets/img/images/logos/elixir.png' ?>" /></li>
-	</ul>
-	<ul class="small-block-grid-2 large-block-grid-4">
-		<li><img src="<?php echo get_template_directory_uri().'/assets/img/images/logos/spector.png' ?>" /></li>
 		<li><img src="<?php echo get_template_directory_uri().'/assets/img/images/logos/gng.png' ?>" /></li>
 		<li><img src="<?php echo get_template_directory_uri().'/assets/img/images/logos/masotti.png' ?>" /></li>
 		<li><img src="<?php echo get_template_directory_uri().'/assets/img/images/logos/shiva.png' ?>" /></li>
+		<li><img src="<?php echo get_template_directory_uri().'/assets/img/images/logos/jager.png' ?>" /></li>
 	</ul>
 </footer>
 <a class="exit-off-canvas"></a>
